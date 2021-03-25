995147c1da50cc1847ec67d522a3aeb4d33b0321
Mitchell Skaggs <skaggsm333@gmail.com>
2021-03-24 20:24:48 -0500

    Clean up and simply new function
    Also add types to as much as possible.

--- functions/index.js
+++ functions/index.js
@@ -23,75 +23,57 @@ exports.helloHttp = (req, res) => {
     }
 };
 
-class coordBuffer {
-    #privateBuffer
+/**
+ * @typedef Coordinate
+ * @type {object}
+ * @property latitude: number
+ * @property longitude: number
+ */
 
-
-    constructor(size) {
-        this.size = size;
-        this.#privateBuffer = {};
-    }
-
-    /**
-     * @param {number | object} id
-     */
-    set addCoord(id, coordData) {
-        if( this.#privateBuffer[id] ) {
-            this.#privateBuffer[id].append(coordData);
-        } else {
-            this.#privateBuffer[id] = [coordData];
-        }
-        this.checkSize(id);
-    }
-
-    get lastCoord(id) {
-        if( this.#privateBuffer[id] ) {
-            return this.#privateBuffer[id].slice(-1)[0];
-        } else {
-            return 0;
-        }
-    }
-
-    checkSize(id) {
-        if( this.#privateBuffer[id].size > this.size ) {
-            let half = Math.ceil(this.#privateBuffer[id].size / 2);
-            this.#privateBuffer[id].splice(0, half);
-        }
-    }
-
-}
-
-let buffer = new coordBuffer(100);
+/**
+ * @type {Object<number, Coordinate>}
+ */
+let coords = {};
 /**
  * @param req {e.Request}
  * @param res {e.Response}
  */
 exports.shuttleCoord = (req, res) => {
-    let coordData = {};
-    let data = '';
-    // TODO - verify authenticity of sender
+    // TODO - verify authenticity of sender: https://cloud.google.com/api-gateway/docs/quickstart-console#securing_access_by_using_an_api_key
+    const {id} = req.query;
     switch (req.method) {
         case 'GET':
-            coordData = buffer.lastCoord();
-            if( coordData == 0 ) {
-                res.status(503).send('Location data not available')
-            }
-            else {
-                res.status(200).send(`Last seen shuttle location ${coordData}`);
-            }
+            getShuttleCoord(req, res, id);
             break;
         case 'POST':
-            req.on('data', chunk => {
-                data += chunk;
-            });
-            req.on('end', () => {
-                coordData = JSON.parse(data);
-            });
-            buffer.addCoord(coordData);
-            res.status(200).send('Success!');
+            postShuttleCoord(req, res, id);
             break;
         default:
             res.status(405).send({error: 'Something blew up!'});
             break;
     }
 };
+
+/**
+ * @param req {e.Request}
+ * @param res {e.Response}
+ * @param id {number}
+ */
+function getShuttleCoord(req, res, id) {
+    let coordData = coords[id];
+    if (coordData) {
+        res.status(200).send(`Last seen shuttle location ${coordData.latitude}, ${coordData.longitude}`);
+    } else {
+        res.status(503).send('Location data not available');
+    }
+}
+
+/**
+ * @param req {e.Request}
+ * @param res {e.Response}
+ * @param id {number}
+ */
+function postShuttleCoord(req, res, id) {
+    coords[id] = req.body;
+    res.status(200).send('Success!');
+}
